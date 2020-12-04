200b01bc617a0845be986088f953815f02e93ac2
Enrique García <enriquegarcia822@gmail.com>
2020-12-04 15:25:14 +0000

    backend de login y creacion de usuario terminado

--- TodoList/models/usuario.js
+++ TodoList/models/usuario.js
@@ -3,9 +3,10 @@ const {Schema} = mongoose;
 
 
 const usuarioSchema = new Schema({
+    userName: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String,  require: true }, 
-    estadoUsuario: {type: Boolean, required: true}   
+    //estadoUsuario: {type: Boolean, required: true}   
 })
 
 
